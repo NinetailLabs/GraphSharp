e26ced72643f0bc1bbed28d24267ae25b40e1024
andypelzer <andy.pelzer@gmx.de>
2018-09-06 20:18:12 +0200

    Added ThemeInfo attribute again. Fixes #19.

--- Graph#.Controls/Properties/AssemblyInfo.cs
+++ Graph#.Controls/Properties/AssemblyInfo.cs
@@ -1,7 +1,10 @@
 using System.Reflection;
+using System.Windows;
 
 [assembly: AssemblyTitle("GraphSharp.Controls")]
 [assembly: AssemblyProduct("GraphSharp")]
 
 [assembly: AssemblyVersion("1.0.4.0")]
 [assembly: AssemblyFileVersion("1.0.4.0")]
+
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
